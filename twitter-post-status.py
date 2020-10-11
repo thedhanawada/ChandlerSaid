56719f9585065b85bacd14a74e016b37f02fee1b
Nirmala Rao Dhanawada <13751641+thedhanawada@users.noreply.github.com>
2020-10-11 11:13:07 +1100

    Update twitter-post-status.py

--- twitter-post-status.py
+++ twitter-post-status.py
@@ -18,6 +18,6 @@ http = urllib3.PoolManager()
 heroes = http.request('GET', 'https://api.sheety.co/98b7e023de93d9f9c9e690887491f1d4/chandler/chandler')
 heroes_dict = json.loads(heroes.data.decode('UTF-8'))
 
-new_status = (heroes_dict['chandler'][randrange(16300)]["chandlerSays"])
+new_status = (heroes_dict['chandlerSays'][randrange(16300)]["chandler"])
 results = twitter.statuses.update(status = new_status+"#FRIENDS"+ " #ChandlerSaid" )
 print("updated status: %s" % new_status)
